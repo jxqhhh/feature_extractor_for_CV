543c6273fc5a83165e76b04f3c6cdaf63e4f648b
金昕祺 <jinxq16@mails.tsinghua.edu.cn>
2018-05-09 11:41:32 +0800

    Add CENTRIST_feature_extractor

---
+++ CENTRIST_feature_extractor/CENTRIST_feature_extractor.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import os
+import sys
+
+def censusTransform(image,x,y):
+	center=image[x][y]
+	neighbours=(image[x-1][y-1],image[x-1][y],image[x-1][y+1],image[x][y-1],image[x][y+1],image[x+1][y-1],image[x+1][y],image[x+1][y+1])
+	result=0
+	for i in range(8):
+		if center>=neighbours[i]:
+			result+=2**(7-i)
+	return result
+
+def censusTransformHistogram(filename):
+	image=cv2.imread(filename,0)
+	width,height=image.shape
+	width-=2
+	height-=2
+
+	ctImage=np.zeros((width,height))
+	for i in range(width):
+		for j in range(height):
+			ctImage[i][j]=censusTransform(image,i+1,j+1)
+
+	hist=np.histogram(ctImage.ravel(),254,[1,254])
+	
+
+if __name__ == '__main__':
+	path=sys.argv[0].split('CENTRIST_feature_extractor.py')[0]
+	num=0
+	for i in os.listdir(path):
+		if i.split('_')[0]=='image':
+			num+=1
+	result=np.zeros((num,254))
+	j=0
+	for i in os.listdir(path):
+		if i.split('_')[0]=='image':
+			result[j,:]=censusTransformHistogram(path+i)
+	np.save("CENTRIST_feature.npy",result)
